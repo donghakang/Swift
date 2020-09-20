0f5b809393c1182475ec79dbfb623a894f6b9b37
Dongha <dkang0602@gmail.com>
2020-09-20 23:50:30 +0900

    [iOS] Starting a photo software for gf

---
+++ Project/Scret/Scret/view/FirstViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FirstViewController.swift
+//  Scret
+//
+//  Created by Dongha Kang on 2020/09/19.
+//  Copyright © 2020 Dongha Kang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    /*
+     미네소타 삶을 위한 폴더
+     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ Project/Scret/Scret/view/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  Scret
+//
+//  Created by Dongha Kang on 2020/09/20.
+//  Copyright © 2020 Dongha Kang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
