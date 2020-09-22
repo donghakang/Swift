45aa740da7bcf3670f8db77e789ad7e5e55f77f1
Dongha <dkang0602@gmail.com>
2020-09-22 11:48:10 +0900

    [iOS] Zig-Zag Collection View

--- Project/Collection View/Collection View/CollectionViewController.swift
+++ Project/Collection View/Collection View/CollectionViewController.swift
@@ -11,7 +11,7 @@ private let reuseIdentifier = "Cell"
 
 class CollectionViewController: UICollectionViewController {
 
-    let countryNames : [String] = ["USA", "Korea", "China", "Japan", "England", "New Zealand", "Vietnam"]
+//    let countryNames : [String] = ["USA", "Korea", "China", "Japan", "England", "New Zealand", "Vietnam"]
     
     let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     let itemsPerRow: CGFloat = 3
@@ -36,18 +36,24 @@ extension CollectionViewController {
     // MARK: UICollectionViewDataSource
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // 아이템 섹션 갯수
-        return 10
+        return 100
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // 아이템 갯수
-        return Int(itemsPerRow)
+//        return Int(itemsPerRow)
+        return 100
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = .black
+        
+        if (indexPath.row % 2 == 0) {
+            cell.backgroundColor = .green
+        } else {
+            cell.backgroundColor = .cyan
+        }
         
         // Configure the cell
         return cell
@@ -67,18 +73,24 @@ extension CollectionViewController : UICollectionViewDelegateFlowLayout {
         //2
         
         
-        let width : CGFloat
-        let height : CGFloat
+        var newSize: CGSize = CGSize.zero
+        newSize.height = 100;
 
-        if indexPath.item == 0 {
-            width = 100
-            height = 100
-        } else {
-            width = 50
-            height = 50
+
+
+        if(indexPath.item % 4 == 0 || indexPath.item % 4 == 3)
+        {
+            // Size : 1/4th of screen
+            newSize.width = view.frame.width * 0.23;
         }
-        
-        return CGSize(width: width, height: height)
+        else
+        {
+            // Size : 3/4th of screen
+            newSize.width = view.frame.width * 0.72;
+
+        }
+        return newSize;
+//        return CGSize(width: width, height: height)
         
         
 //        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
@@ -88,17 +100,17 @@ extension CollectionViewController : UICollectionViewDelegateFlowLayout {
 //        return CGSize(width: widthPerItem, height: widthPerItem)
     }
     
-    //3
-      func collectionView(_ collectionView: UICollectionView,
-                          layout collectionViewLayout: UICollectionViewLayout,
-                          insetForSectionAt section: Int) -> UIEdgeInsets {
-        return sectionInsets
-      }
-      
+//    //3
+//      func collectionView(_ collectionView: UICollectionView,
+//                          layout collectionViewLayout: UICollectionViewLayout,
+//                          insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return sectionInsets
+//      }
+//
       // 4
-      func collectionView(_ collectionView: UICollectionView,
-                          layout collectionViewLayout: UICollectionViewLayout,
-                          minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return sectionInsets.left
-      }
+//      func collectionView(_ collectionView: UICollectionView,
+//                          layout collectionViewLayout: UICollectionViewLayout,
+//                          minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 10
+//      }
 }
