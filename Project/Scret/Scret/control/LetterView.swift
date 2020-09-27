5023a6ca38589e5cc159ca0ae70b6a6341823b61
Dongha <dkang0602@gmail.com>
2020-09-27 18:13:33 +0900

    [iOS] Collection Reusable View Controller

---
+++ Project/Scret/Scret/control/LetterView.swift
@@ -0,0 +1,14 @@
+//
+//  LetterView.swift
+//  Scret
+//
+//  Created by Dongha Kang on 2020/09/27.
+//  Copyright © 2020 Dongha Kang. All rights reserved.
+//
+
+import UIKit
+
+class LetterView: UICollectionReusableView {
+
+    
+}
--- Project/Scret/Scret/view/SecondViewController.swift
+++ Project/Scret/Scret/view/SecondViewController.swift
@@ -12,16 +12,55 @@ import Photos
 class SecondViewController: UICollectionViewController {
     
     var assets = [PHAsset]()
-    
     var imageArray = [UIImage]()
+    
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupCollectionView()
+        
+        addGesture()
+    }
 
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        if collectionView.numberOfItems(inSection: 0) > 0 {
+//            collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: false)
+//        }
+//    }
+
+    
+    
+
+    // MARK: Gesture Recognition
+    func addGesture() {
+        let backSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler(sender:)))
+        backSwipeGesture.direction = .right
+        let downSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler(sender:)))
+        downSwipeGesture.direction = .down
+        self.view.addGestureRecognizer(backSwipeGesture)
+        self.view.addGestureRecognizer(downSwipeGesture)
+        
+    }
+    
+    @objc func swipeHandler(sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            if sender.direction == .right {
+                _ = navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+
+    
+    
+    
+    // setup
+    func setupCollectionView() {
         // Setup the mosaic collection view.
         let mosaicLayout = MosaicLayout()
         collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: mosaicLayout)
-        collectionView.backgroundColor = UIColor.appBackgroundColor
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.alwaysBounceVertical = true
         collectionView.indicatorStyle = .white
@@ -33,14 +72,29 @@ class SecondViewController: UICollectionViewController {
         
         imageSetup()
     }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if collectionView.numberOfItems(inSection: 0) > 0 {
-            collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: false)
-        }
+    
+    
+    func setupTextView() {
+        let textview = UITextView()
+        textview.translatesAutoresizingMaskIntoConstraints = false
+        textview.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        textview.heightAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true // Y 사이즈
+        textview.widthAnchor.constraint(equalToConstant: self.view.frame.width).isActive = true  // X 사이즈
+           
+        textview.text = "HELLO WORLD"
+        textview.backgroundColor = .cyan
+        
+        self.view.addSubview(textview)
     }
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // MARK: UICollectionViewDataSource
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -60,14 +114,21 @@ class SecondViewController: UICollectionViewController {
         return cell
     }
     
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let letterView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "myLetter", for: indexPath) as! LetterView
+        
+        letterView.backgroundColor = .cyan
+        return letterView
+    }
     
     // MARK: Images
     func imageSetup() {
         for i in 0..<11 {
             imageArray.append(UIImage(named: "BA/" + String(i))!)
-
         }
     }
+    
+    
     
 
 }
