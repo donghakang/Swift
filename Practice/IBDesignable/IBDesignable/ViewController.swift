57297bfac486b2f597084fa6a5c0c53e6c0b34f4
Dongha <dkang0602@gmail.com>
2020-10-05 17:41:46 +0900

    [Organize] Folder movement to separate practice and project

---
+++ Practice/IBDesignable/IBDesignable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBDesignable
+//
+//  Created by Dongha Kang on 2020/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
